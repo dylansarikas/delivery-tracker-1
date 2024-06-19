e57520091982413e8ca046649932d2c32d475d35
dylansarikas <dylan.sarikas@gmail.com>
2024-06-19 13:29:53 -0500

    runs db migrations

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id          :integer          not null, primary key
+#  arrival     :date
+#  description :string
+#  details     :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
 class Delivery < ApplicationRecord
 end
